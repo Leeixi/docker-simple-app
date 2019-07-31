8a397360c5d8b3d7f39189c501828d6a32480812
David <david.lee555@hotmail.com>
2019-07-31 12:42:42 +0200

    new file:   ca.crt 	modified:   docker_push.sh

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+sudo mkdir -p /etc/docker/certs.d/116.202.28.78:5000
+sudo chmod -R 700 /etc/docker/certs.d/116.202.28.78:5000
+sudo cp ca.crt /etc/docker/certs.d/116.202.28.78:5000
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USERNAME" --password-stdin https://116.202.28.78:5000/
 docker tag docker-test 116.202.28.78:5000/docker-test
-docker docker-test 116.202.28.78:5000/docker-test
-docker 116.202.28.78:5000/docker-test
+docker push docker-test 116.202.28.78:5000/docker-test
