4e8be749361b5ba90d7ee70c19d647b0a5d752e5
David <david.lee555@hotmail.com>
2019-07-31 12:35:24 +0200

    modified:   docker_push.sh

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "$DOCKER_PASS" | docker login -u "$DOCKER_USERNAME" --password-stdin
-https://116.202.28.78:5000/
+echo "$DOCKER_PASS" | docker login -u "$DOCKER_USERNAME" --password-stdin https://116.202.28.78:5000/
 docker tag docker-test 116.202.28.78:5000/docker-test
 docker docker-test 116.202.28.78:5000/docker-test
+docker 116.202.28.78:5000/docker-test
